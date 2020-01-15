46481ae95135289a89e0b731ff5be49d53c3662f
Becode <stevekeersma@gmail.com>
2020-01-15 12:21:43 +0100

    added evolution link2

--- script.js
+++ script.js
@@ -3,37 +3,44 @@ let input = document.getElementById("pokemon");
 let pokemonImage = document.getElementById('pokeImg');
 
 
-
-
 button.addEventListener('click', function () {
 
 
-fetch('https://pokeapi.co/api/v2/pokemon/'+input.value.toLowerCase() +'')
-    .then(link => link.json())
-    .then(data => {
-        console.log(data.sprites.front_default);
-        let pokeImageSource = (data.sprites.front_default);
-        pokemonImage.setAttribute('src', pokeImageSource);
-        let id=data.id;
-        document.getElementById("pokeId").innerHTML=id;
+    fetch('https://pokeapi.co/api/v2/pokemon/' + input.value.toLowerCase() + '')
+        .then(link => link.json())
+        .then(data => {
+            console.log(data.sprites.front_default);
+            let pokeImageSource = (data.sprites.front_default);
+            pokemonImage.setAttribute('src', pokeImageSource);
+            let id = data.id;
+            document.getElementById("pokeId").innerHTML = id;
 
             let pokeName = data.name.charAt(0).toUpperCase() + data.name.slice(1);
             console.log(pokeName);
             document.getElementById('pokeName').innerHTML = pokeName;
             let pokemonId = document.getElementById("pokeId").innerText;
 
-            fetch('https://pokeapi.co/api/v2/pokemon-species/'+pokemonId+'')
+            fetch('https://pokeapi.co/api/v2/pokemon-species/' + pokemonId + '')
                 .then(link => link.json())
                 .then(data => {
 
-                        console.log(data)
+                    console.log(data);
+                    console.log(data.evolution_chain.url);
+                    let evolutionUrl = data.evolution_chain.url;
+                    fetch(evolutionUrl)
+                        .then(link => link.json())
+                        .then(data => {
+
+                            console.log(data);
+
+
+                        });
 
 
                 });
 
 
-    });
-
+        });
 
 
 });
