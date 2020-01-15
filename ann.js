c8298def90b768923530598717bdfb5082a2b44e
AnnKemp <ann.kemp@scarlet.be>
2020-01-15 10:59:04 +0100

    update link (case) in file

--- ann.js
+++ ann.js
@@ -1,12 +1,10 @@
 let button = document.getElementById('run');
 let input = document.getElementById("pokemon");
 
-
-
 button.addEventListener('click', function () {
 
 // ge zoekt op het beestje dat je ingeeft en krijgt dan die json lijst van dat specifiek beestje
-    fetch('https://pokeapi.co/api/v2/pokemon/'+input.value +'')
+    fetch('https://pokeapi.co/api/v2/pokemon/'+input.value.toLowerCase() +'')
         .then(link => link.json())
         .then(data => {
            // console.log(data);
