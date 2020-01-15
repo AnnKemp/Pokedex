4aa8e63246dbc649e2bb9e1a90ff0510e003b091
AnnKemp <ann.kemp@scarlet.be>
2020-01-15 10:52:04 +0100

    id added

---
+++ ann.js
@@ -0,0 +1,19 @@
+let button = document.getElementById('run');
+let input = document.getElementById("pokemon");
+
+
+
+button.addEventListener('click', function () {
+
+// ge zoekt op het beestje dat je ingeeft en krijgt dan die json lijst van dat specifiek beestje
+    fetch('https://pokeapi.co/api/v2/pokemon/'+input.value +'')
+        .then(link => link.json())
+        .then(data => {
+           // console.log(data);
+
+            let id=data.id;
+            document.getElementById("pokeId").innerHTML="Poke ID: "+id;
+
+
+        });
+});
