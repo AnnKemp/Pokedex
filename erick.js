0966fc4872d7ff814a4f4165796352a009d783a7
Erick Medina <erickmedinabe@gmail.com>
2020-01-16 16:44:11 +0100

    enter added

--- erick.js
+++ erick.js
@@ -1,64 +1,67 @@
 let button = document.getElementById('run');
 let input = document.getElementById("pokemon");
 
-button.addEventListener('click', function () {
+input.addEventListener('keyup', function (e) {
 
-    fetch('https://pokeapi.co/api/v2/pokemon/' + input.value + '')
-        .then(link => link.json())
-        .then(data => {
-            console.log(data);
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            button.click();
 
-            // poke name
-            let pokeName = data.name.charAt(0).toUpperCase() + data.name.slice(1);
-            //console.log(pokeName);
-            document.getElementById('pokeName').innerHTML = pokeName;
+            fetch('https://pokeapi.co/api/v2/pokemon/' + input.value + '')
+                .then(link => link.json())
+                .then(data => {
+                    console.log(data);
 
-            // weight
-            let pokeWeight = data.weight;
-            document.getElementById('weightPoke').innerHTML = ('Weight: ' + pokeWeight);
+                    // poke name
+                    let pokeName = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+                    //console.log(pokeName);
+                    document.getElementById('pokeName').innerHTML = pokeName;
 
-            // abilities
-            let abilitiesNewArray = [];
-            let pokeAbilities;
+                    // weight
+                    let pokeWeight = data.weight;
+                    document.getElementById('weightPoke').innerHTML = ('Weight: ' + pokeWeight);
 
-            for (let i = 0; i < data.abilities.length; i++) { // to get all elements from the array
-                pokeAbilities = abilitiesNewArray.push(data.abilities[i].ability.name); // to add new array + to select abilities specifically from the array
-                pokeAbilities = abilitiesNewArray[Math.floor(Math.random() * abilitiesNewArray.length)]; // to make the selection randomly
-                document.getElementById('abilitiesPoke').innerHTML = ('Abilities: ' + pokeAbilities); // to get element id from the html and display new array
-            }
+                    // abilities
+                    let abilitiesNewArray = [];
+                    let pokeAbilities;
 
-            // types
+                    for (let i = 0; i < data.abilities.length; i++) { // to get all elements from the array
+                        pokeAbilities = abilitiesNewArray.push(data.abilities[i].ability.name); // to add new array + to select abilities specifically from the array
+                        pokeAbilities = abilitiesNewArray[Math.floor(Math.random() * abilitiesNewArray.length)]; // to make the selection randomly
+                        document.getElementById('abilitiesPoke').innerHTML = ('Abilities: ' + pokeAbilities); // to get element id from the html and display new array
+                    }
 
-            var pokeTypes1 = data.types[0].type.name;
+                    // types
+                    var pokeTypes1 = data.types[0].type.name;
 
-            if (data.types.length ===1) {
-                document.getElementById("pokeTypes1").innerHTML= pokeTypes1;
-                document.getElementById("pokeTypes2").innerHTML= " ";
-            } else {
-                var pokeTypes2 = data.types[1].type.name;
-                document.getElementById("pokeTypes1").innerHTML= pokeTypes1;
-                document.getElementById("pokeTypes2").innerHTML= pokeTypes2;
-            }
+                    if (data.types.length === 1) {
+                        document.getElementById("pokeTypes1").innerHTML = pokeTypes1;
+                        document.getElementById("pokeTypes2").innerHTML = " ";
+                    } else {
+                        var pokeTypes2 = data.types[1].type.name;
+                        document.getElementById("pokeTypes1").innerHTML = pokeTypes1;
+                        document.getElementById("pokeTypes2").innerHTML = pokeTypes2;
+                    }
 
-            // poke moves
-            let pokeMovesNewArray = []; // empty array for poke moves
+                    // poke moves
+                    let pokeMovesNewArray = []; // empty array for poke moves
 
-            for (let i = 0; i < data.moves.length; i++) {
-                pokeMovesNewArray.push(data.moves[i].move.name);
-            }
+                    for (let i = 0; i < data.moves.length; i++) {
+                        pokeMovesNewArray.push(data.moves[i].move.name);
+                    }
 
-            let randomMove;
-            let randomFourMoves;
+                    let randomMove;
+                    let randomFourMoves;
 
-            for (let i = 0; i < 4; i++) {
-                randomMove = Math.floor(Math.random() * pokeMovesNewArray.length);
-                randomFourMoves = pokeMovesNewArray[randomMove];
-                //console.log(randomFourMoves);
-                document.getElementById('pokeMove' + (i + 1) + '').innerHTML = randomFourMoves;
-            }
+                    for (let i = 0; i < 4; i++) {
+                        randomMove = Math.floor(Math.random() * pokeMovesNewArray.length);
+                        randomFourMoves = pokeMovesNewArray[randomMove];
+                        //console.log(randomFourMoves);
+                        document.getElementById('pokeMove' + (i + 1) + '').innerHTML = randomFourMoves;
+                    }
 
-
-        });
+                });
+        }
 });
 
 /*
