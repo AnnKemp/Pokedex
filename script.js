40e7f4b754d93402a01d85d8f199e11b4e254a32
Becode <stevekeersma@gmail.com>
2020-01-16 10:08:09 +0100

    added shiny

--- script.js
+++ script.js
@@ -2,27 +2,29 @@ let button = document.getElementById('run');
 let input = document.getElementById("pokemon");
 let pokemonImage = document.getElementById('pokeImg');
 let evoImage = document.getElementById('evoImg');
+let shinyImage = document.getElementById('shinyImg');
 
 button.addEventListener('click', function () {
 
-getPrevo();
+    getPrevo();
+    getShiny();
     fetch('https://pokeapi.co/api/v2/pokemon/' + input.value.toLowerCase() + '')
         .then(link => link.json())
         .then(data => {
             //console.log(data);
-           // console.log(data.sprites.front_default);
+            // console.log(data.sprites.front_default);
             let pokeImageSource = (data.sprites.front_default);
             pokemonImage.setAttribute('src', pokeImageSource);
             let id = data.id;
             document.getElementById("pokeId").innerHTML = id;
 
             let pokeName = data.name.charAt(0).toUpperCase() + data.name.slice(1);
-           // console.log(pokeName);
+            // console.log(pokeName);
             document.getElementById('pokeName').innerHTML = pokeName;
             let pokemonId = document.getElementById("pokeId").innerText;
 
             // poke moves
-            let pokeMovesNewArray= []; // empty array for poke moves
+            let pokeMovesNewArray = []; // empty array for poke moves
 
             for (let i = 0; i < data.moves.length; i++) {
                 pokeMovesNewArray.push(data.moves[i].move.name);
@@ -32,43 +34,40 @@ getPrevo();
             let randomFourMoves;
 
             for (let i = 0; i < 4; i++) {
-                randomMove= Math.floor(Math.random()*pokeMovesNewArray.length);
+                randomMove = Math.floor(Math.random() * pokeMovesNewArray.length);
                 randomFourMoves = pokeMovesNewArray[randomMove];
-               // console.log(randomFourMoves);
-                document.getElementById('pokeMove'+(i+1)+'').innerHTML = randomFourMoves;
+                // console.log(randomFourMoves);
+                document.getElementById('pokeMove' + (i + 1) + '').innerHTML = randomFourMoves;
             }
 
             let pokeWeight = data.weight;
-            document.getElementById('weightPoke').innerHTML= ('Weight: ' + pokeWeight);
+            document.getElementById('weightPoke').innerHTML = ('Weight: ' + pokeWeight);
 
 
-                })
+        })
 
 
-
-        });
-
+});
 
 
 async function getPrevo() {
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${input.value}`);
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${input.value.toLowerCase()}`);
     let evolutionData = await response.json();
 
     if (evolutionData.evolves_from_species == null) {
         document.getElementById('prevEvolution').innerHTML = "";
         evoImage.setAttribute("src", "")
-    }
-    else {
+    } else {
         const preName = evolutionData.evolves_from_species.name;
         document.getElementById('prevEvolution').innerHTML = "previous evolution: " + preName;
         preForm(preName);
     }
 }
 
-async function preForm(namepokemon) {
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${namepokemon}`);
+async function preForm(prevolution) {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${prevolution}`);
     let preData = await response.json();
-    console.log(preData.sprites);
+    //console.log(preData.sprites);
     let pokemonSprite = preData.sprites.front_default;
 
     evoImage.setAttribute("src", pokemonSprite);
@@ -76,8 +75,16 @@ async function preForm(namepokemon) {
 }
 
 
+async function getShiny() {
 
 
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${input.value.toLowerCase()}`);
+    let shinyData = await response.json();
+    let shinySprite = shinyData.sprites.front_shiny;
+    shinyImage.setAttribute("src", shinySprite);
+
+
+}
 
 
 // //fetch next evo
